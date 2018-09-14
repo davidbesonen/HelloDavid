b5064e94b7a3ebdc159cfe490d34e8b795ef6d90
davidbesonen <42583281+davidbesonen@users.noreply.github.com>
2018-09-14 16:02:20 -0500

    Create HelloDavid.c

---
+++ HelloDavid.c
@@ -0,0 +1,13 @@
+/**
+* Author: David Besonen
+* Date: September 13, 2018
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv){
+  
+  printf("Hello David!");
+  return 0;
+ }
